da155a1f72ad23a5184d84f966cc28a3861a9c57
Scott McCain <scott.mccain@gmail.com>
2016-05-04 17:57:44 +0000

    Started fleshing out WebAudio Controls

---
+++ app/source/js/controller/index.js
@@ -0,0 +1,5 @@
+'use strict';
+var app = require('angular').module('angular-application');
+
+app.controller('HomeController', require('./home'));
+app.controller('AboutController', require('./about'));
---
+++ app/source/js/directive/index.js
@@ -0,0 +1,4 @@
+'use strict';
+var app = require('angular').module('angular-application');
+
+app.directive('WebaudioKnob', require('./webaudioKnob'));
